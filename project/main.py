db146160ed0e2e73ce0a4440b4767f017c3d9c4c
shureck <Sashalev200149@gmail.com>
2021-07-25 12:41:44 +0300

    init commit

---
+++ project/main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, status, Header, Request, Depends
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+@app.get("/")
+async def getAll():
+    return "all"
